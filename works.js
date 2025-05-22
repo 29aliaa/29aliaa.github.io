8bc3e1f2348790516ed3b115d7fb72969d68bb9a
29aliaa <aliaserene@gmail.com>
2025-05-22 02:09:59 -0500

    click to enlarge

--- works.js
+++ works.js
@@ -98,3 +98,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+function enlarge(imgElement) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
